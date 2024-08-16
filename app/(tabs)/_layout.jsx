dc152484e1caac22e0d6ac64f833354be3168da5
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-08-16 17:24:13 -0400

    'react navtive project'

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,47 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router';
+
+import { icons } from '../../constants';
+
+
+const TabIcon = ({icon, color, name, focused }) => {
+    return ( 
+        <View>
+            <Image
+                source={icon}
+                resizeMode='contain'
+                tintColor={color}
+                className="w-6 h-6"
+            />
+            <Text className={ '${focus'}>
+                [name]
+            </Text>
+        </View>
+    )
+}
+const TabsLayout = () => {
+  return (
+    <> 
+    <Tabs>
+        <Tabs.Screen
+        name='home'
+        options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({color, focus}) => (
+                <TabIcon 
+                icon={icons.home}
+                color={color}
+                name='Home'
+                focused={focus}
+                /> 
+            )
+        }}
+        />
+    </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
