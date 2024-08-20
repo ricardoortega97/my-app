f8ef6b37ba5cb6b5747d7b3eb5353f2d047bb47e
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2024-08-20 17:01:49 -0400

    'page layout'

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,47 +1,107 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router';
+import { Tabs, Redirect } from 'expo-router';
 
 import { icons } from '../../constants';
-
-
-const TabIcon = ({icon, color, name, focused }) => {
-    return ( 
-        <View>
-            <Image
+//the Icon on the tabs design and adjustments
+const TabIcon = ({icon, color, name, focused}) => {
+    return (
+        <View className="items-center justify-center gap-2">
+            <Image  
                 source={icon}
                 resizeMode='contain'
                 tintColor={color}
-                className="w-6 h-6"
+                className='w-6 h-6'
             />
-            <Text className={ '${focus'}>
-                [name]
+            <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} 
+            style={{color : color}}>
+                {name}
             </Text>
         </View>
     )
 }
+
 const TabsLayout = () => {
-  return (
-    <> 
-    <Tabs>
-        <Tabs.Screen
-        name='home'
-        options={{
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({color, focus}) => (
-                <TabIcon 
-                icon={icons.home}
-                color={color}
-                name='Home'
-                focused={focus}
-                /> 
-            )
-        }}
-        />
-    </Tabs>
-    </>
-  )
+    return (
+        <> 
+            <Tabs
+            screenOptions={{
+                tabBarShowLabel : false,
+                ///when the tab button is called, it will be highlighted yellow 
+                tabBarActiveTintColor : '#FFA001',
+                tabBarInactiveTintColor: '#CDCDE0',
+                tabBarStyle: {
+                    backgroundColor : '#161622',
+                    borderTopWidth : 1,
+                    borderTopColor : '#232533',
+                    height : 84
+                }
+                }}
+            >
+                <Tabs.Screen
+                ///calls the layout of the tabs folder
+                name="home"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        //call of the function from the top line 
+                        <TabIcon 
+                        icon={icons.home}
+                        color={color}
+                        name='Home'
+                        focused={focused}
+                        /> 
+                    )
+                }}
+                />
+                <Tabs.Screen
+                name='bookmarks'
+                options={{
+                    title: 'Bookmark',
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        <TabIcon 
+                        icon={icons.bookmark}
+                        color={color}
+                        name='Bookmark'
+                        focused={focused}
+                        /> 
+                    )
+                }}
+                />
+                <Tabs.Screen
+                name='create'
+                options={{
+                    title: 'Create',
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        <TabIcon 
+                        icon={icons.plus}
+                        color={color}
+                        name='Create'
+                        focused={focused}
+                        /> 
+                    )
+                }}
+                />
+                <Tabs.Screen
+                name='profile'
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({color, focused}) => (
+                        <TabIcon 
+                        icon={icons.profile}
+                        color={color}
+                        name='profile'
+                        focused={focused}
+                        /> 
+                    )
+                }}
+            />
+        </Tabs>
+        </>
+    )
 }
 
 export default TabsLayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { Ridirect, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text >Hello world</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style ={{color: 'blue'}}>Go to Profile</Link>
-    </View>
+  return ( 
+    ///Doesn't overlap any bars from devices 
+    <SafeAreaView className='bg-purple-400 h-full'>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className='w-full justify-center items-center min-h-[85wh] px-4'> 
+          <Image 
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
+          <Image 
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless: {' '} 
+              <Text className='text-secondary-200'>
+                  Aora
+              </Text>
+            </Text>
+            <Image 
+              source={images.path}
+              className='w-[136px] h-[15] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+          <Text className='text-gray-100 mt-7 text-center'>
+            Where creativitiy meets innovation:
+            embark on a journey of limitless exporation
+          </Text> 
+          
+          <CustomButton 
+          title='Continue with Email'
+          handlepress={() => router.push('/sign-in')}
+          containerStyles={"w-full mt-7"}
+          />
+        </View>
+      </ScrollView>
+      <StatusBar 
+        backgroundColor='#162622' style='light'
+      />
+    </SafeAreaView>
   );
 }
 
